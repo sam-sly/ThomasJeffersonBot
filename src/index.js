e971a1f0feb672c217bc1395e8f8657de77eb1d4
sam-sly <samsly7@gmail.com>
2023-10-09 21:07:25 -0600

    Initialized bot

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 const { Client, IntentsBitField } = require('discord.js')
 
+const bot = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ]
+})
+
+bot.login(
+  'MTE2MTA1MzA0ODgyMzYyMzc5Mw.GfpUQ_.l0PKtaRY8G5x1dYqkj7gLbswsbmddeics5CKPY'
+)
