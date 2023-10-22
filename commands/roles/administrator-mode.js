a0729144451fa9c1973410b66dfc7045f9438bc7
sam-sly <samsly7@gmail.com>
2023-10-21 19:45:54 -0600

    Added administrator mode command

---
+++ commands/roles/administrator-mode.js
@@ -0,0 +1,71 @@
+const {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  PermissionsBitField,
+  Role,
+} = require("discord.js");
+const getMembersRole = require("../../utils/getMembersRole");
+
+const ADMIN = 4;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('administrator-mode')
+    .setDescription('Toggle administrator mode for only you.'),
+
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   */
+  execute: async (interaction) => {
+    const member = interaction.member;
+
+    const { value: usersRole } = await getMembersRole(member);
+
+    if (usersRole < ADMIN) {
+      await interaction.editReply({
+        content: `You don't have permission to use this command.`,
+      });
+      return;
+    }
+
+    let administratorStatus;
+    /**
+     * @type {Role} administratorRole
+     */
+    let administratorRole = await member.roles.cache.find((r) => r.name === 'Administrator');
+
+    if (administratorRole === undefined) {
+      administratorRole = interaction.guild.roles.cache.find((r) => r.name === 'Administrator');
+      
+      if (administratorRole === undefined) {
+        administratorRole = await interaction.guild.roles.create({
+          name: 'Administrator',
+          permissions: [ PermissionsBitField.Flags.Administrator ],
+        });
+      }
+
+      await member.roles.add(administratorRole);
+      administratorStatus = 'on';
+    }
+    else {
+      await member.roles.remove(administratorRole);
+
+      const otherMembersInAdministratorMode = interaction.guild.members.cache.find(
+        (m) => m.roles.cache.find((r) => r.name === 'Administrator')
+      );
+
+      if (!otherMembersInAdministratorMode) {
+        await administratorRole.delete();
+      }
+
+      administratorStatus = 'off';
+    }
+
+    console.log(`Administrator mode is ${administratorStatus} for ${interaction.member.displayName}.`);
+    await interaction.editReply({
+      content: `Administrator mode is **${administratorStatus}**.`,
+    });
+
+    // TODO: maybe add timeout for administrator role
+  },
+};
