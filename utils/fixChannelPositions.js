84846037ab4b1c36bbf3f5eccb5b471097328f57
sam-sly <samsly7@gmail.com>
2024-02-26 14:56:12 -0700

    Empty social channels now gets sorted to end on resets

--- utils/fixChannelPositions.js
+++ utils/fixChannelPositions.js
@@ -2,12 +2,23 @@ module.exports = async (category, socialChannels, gamingChannels) => {
   const membersChannels = category.children.cache.filter(ch => ch.isVoiceBased());
 
   let i = 0;
+  let emptyChannel = null;
   
   for (const [id, channel] of membersChannels) {
     if (!socialChannels.some(s => s.id === id)) continue;
 
+    if (channel.members.size === 0) {
+      emptyChannel = channel;
+      continue;
+    }
+
     await channel.setPosition(i++);
-    console.log(`Moved "${channel.name}" to position ${i}.`);
+    console.log(`Moved "${channel.name}" to position ${i-1}.`);
+  }
+
+  if (emptyChannel) {
+    await emptyChannel.setPosition(i++);
+    console.log(`Moved "${emptyChannel.name}" to position ${i-1}.`);
   }
 
   for (const g of gamingChannels) {
@@ -16,7 +27,7 @@ module.exports = async (category, socialChannels, gamingChannels) => {
     if (!channel) continue;
 
     await channel.setPosition(i++);
-    console.log(`Moved "${channel.name}" to position ${i}.`);
+    console.log(`Moved "${channel.name}" to position ${i-1}.`);
   }
 
   return;
