f07e414708b80aed7b9e080ae8878f6b48bdea41
sam-sly <samsly7@gmail.com>
2024-04-04 23:35:02 -0600

    Added logs to debug startup

--- handlers/refreshFollowGames.js
+++ handlers/refreshFollowGames.js
@@ -9,19 +9,19 @@ const Game = require('../models/game');
 
 const SILENT_FLAG = 4096;
 
-const shift = (table, i, num) => {
-  let j = table[i].length - 1;
+const shift = (messages, i, num) => {
+  let j = messages[i].length - 1;
 
   do {
-    const move = table[i][j].splice(table[i][j].length - num, num);
+    const move = messages[i][j].splice(messages[i][j].length - num, num);
 
-    if (j < table[i].length - 1) j++;
+    if (j < messages[i].length - 1) j++;
     else { j = 0; i++; }
 
-    table[i][j].unshift(move);
-  } while (table[i][j].length > 5);
+    messages[i][j].unshift(move);
+  } while (messages[i][j].length > 5);
 
-  return table;
+  return messages;
 };
 
 const groupGames = (games) => {
@@ -32,6 +32,7 @@ const groupGames = (games) => {
   const sortedGames = games.sort((a, b) => {
     return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
   });
+
   for (const game of sortedGames) {
     rowOfGames.push(game);
 
@@ -45,35 +46,49 @@ const groupGames = (games) => {
       }
     }
   }
+
   if (rowOfGames.length > 0) {
     messageOfRows.push(rowOfGames);
     messages.push(messageOfRows);
   }
 
-  for (let i = 0; i < messages.length - 1; i++) {
-    const firstLetterOfLastGameOfThisMessage = messages[i][messages[i].length-1][messages[i][messages[i].length-1].length-1].name.toLowerCase().charAt(0);
-    const firstLetterOfFirstGameOfNextMessage = messages[i+1][0][0].name.toLowerCase().charAt(0);
-    if (firstLetterOfLastGameOfThisMessage === firstLetterOfFirstGameOfNextMessage) {
-      let count = 0;
-      let firstLetterOfNextGameBack;
-      do {
-        count++;
-        firstLetterOfNextGameBack = messages[i][messages[i].length-1][messages[i][messages[i].length-1].length-1].name.toLowerCase().charAt(0);
-      } while (firstLetterOfNextGameBack === firstLetterOfFirstGameOfNextMessage);
-      messages = shift(messages, i, count);
-    }
-  }
+  console.log(messages);
+
+  // for (let i = 0; i < messages.length - 1; i++) {
+  //   const lastRowOfThisMessage = messages[i][messages[i].length-1];
+  //   const lastGameOfThisMessage = lastRowOfThisMessage[lastRowOfThisMessage.length-1].name;
+  //   const firstGameOfNextMessage = messages[i+1][0][0].name;
+  //   const firstLetterOfLastGameOfThisMessage = lastGameOfThisMessage.toLowerCase().charAt(0);
+  //   const firstLetterOfFirstGameOfNextMessage = firstGameOfNextMessage.toLowerCase().charAt(0);
+  //   console.log(lastGameOfThisMessage, firstGameOfNextMessage);
+  //   if (firstLetterOfLastGameOfThisMessage === firstLetterOfFirstGameOfNextMessage) {
+  //     let count = 0;
+  //     let firstLetterOfNextGameBack;
+  //     do {
+  //       count++;
+  //       const nextGameBack = lastRowOfThisMessage[lastRowOfThisMessage.length-count-1].name;
+  //       console.log(nextGameBack);
+  //       firstLetterOfNextGameBack = nextGameBack.toLowerCase().charAt(0);
+  //     } while (firstLetterOfNextGameBack === firstLetterOfFirstGameOfNextMessage);
+  //     messages = shift(messages, i, count);
+  //   }
+  // }
+  //
+  // console.log(messages);
 
   return messages;
 };
 
 module.exports = async (guild) => {
+  console.log('Refreshing follow-games...')
+
   const games = await Game.find().exec();
   const groupedGames = groupGames(games);
 
   const followGamesChannel = guild.channels.cache.get(process.env.FOLLOW_GAMES_CHANNEL);
   const messagesMap = await followGamesChannel.messages.fetch();
-  const messages = Array.from(messagesMap, ([id, message]) => (message)).reverse();
+  const messages = Array.from(messagesMap, ([_, message]) => (message)).reverse();
+  console.log(messages);
 
   console.log('Updating follow-games header message...');
 
